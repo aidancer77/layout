42cbaf3368a4ce1e701a20016df577e4d0ce056f
Aida <bluebonnets77@gmail.com>
2025-07-18 17:04:16 +0300

    5

--- js/javascript.js
+++ js/javascript.js
@@ -2,9 +2,11 @@
 function vol4_1() {
   const volume4_1 = document.getElementById('volume4_1');
   const checkVol4_1 = document.getElementById('checkVol4_1');
+
   if (checkVol4_1.checked == true) {
     volume4_1.classList.add('d-block');
     volume4_1.classList.remove('d-none');
+    volume4_1.style.height="50%";
   } else {
     volume4_1.classList.add('d-none');
     volume4_1.classList.remove('d-block');
@@ -14,9 +16,11 @@ function vol4_1() {
 function vol4_2() {
   const volume4_2 = document.getElementById('volume4_2');
   const checkVol4_2 = document.getElementById('checkVol4_2');
+
   if (checkVol4_2.checked == true) {
     volume4_2.classList.add('d-block');
     volume4_2.classList.remove('d-none');
+    volume4_2.style.height="15%";
   } else {
     volume4_2.classList.add('d-none');
     volume4_2.classList.remove('d-block');
@@ -26,9 +30,11 @@ function vol4_2() {
 function vol4_3() {
   const volume4_3 = document.getElementById('volume4_3');
   const checkVol4_3 = document.getElementById('checkVol4_3');
+
   if (checkVol4_3.checked == true) {
     volume4_3.classList.add('d-block');
     volume4_3.classList.remove('d-none');
+    volume4_3.style.height="20%";
   } else {
     volume4_3.classList.add('d-none');
     volume4_3.classList.remove('d-block');
@@ -38,9 +44,11 @@ function vol4_3() {
 function vol4_4() {
   const volume4_4 = document.getElementById('volume4_4');
   const checkVol4_4 = document.getElementById('checkVol4_4');
+
   if (checkVol4_4.checked == true) {
     volume4_4.classList.add('d-block');
     volume4_4.classList.remove('d-none');
+    volume4_4.style.height="25%";
   } else {
     volume4_4.classList.add('d-none');
     volume4_4.classList.remove('d-block');
@@ -50,9 +58,11 @@ function vol4_4() {
 function vol4_5() {
   const volume4_5 = document.getElementById('volume4_5');
   const checkVol4_5 = document.getElementById('checkVol4_5');
+
   if (checkVol4_5.checked == true) {
     volume4_5.classList.add('d-block');
     volume4_5.classList.remove('d-none');
+    volume4_5.style.height="30%";
   } else {
     volume4_5.classList.add('d-none');
     volume4_5.classList.remove('d-block');
@@ -62,9 +72,11 @@ function vol4_5() {
 function vol4_6() {
   const volume4_6 = document.getElementById('volume4_6');
   const checkVol4_6 = document.getElementById('checkVol4_6');
+
   if (checkVol4_6.checked == true) {
     volume4_6.classList.add('d-block');
     volume4_6.classList.remove('d-none');
+    volume4_6.style.height="35%";
   } else {
     volume4_6.classList.add('d-none');
     volume4_6.classList.remove('d-block');
@@ -74,9 +86,11 @@ function vol4_6() {
 function vol4_7() {
   const volume4_7 = document.getElementById('volume4_7');
   const checkVol4_7 = document.getElementById('checkVol4_7');
+
   if (checkVol4_7.checked == true) {
     volume4_7.classList.add('d-block');
     volume4_7.classList.remove('d-none');
+    volume4_7.style.height="40%";
   } else {
     volume4_7.classList.add('d-none');
     volume4_7.classList.remove('d-block');
@@ -86,9 +100,11 @@ function vol4_7() {
 function vol4_8() {
   const volume4_8 = document.getElementById('volume4_8');
   const checkVol4_8 = document.getElementById('checkVol4_8');
+
   if (checkVol4_8.checked == true) {
     volume4_8.classList.add('d-block');
     volume4_8.classList.remove('d-none');
+    volume4_8.style.height="45%";
   } else {
     volume4_8.classList.add('d-none');
     volume4_8.classList.remove('d-block');
@@ -98,9 +114,11 @@ function vol4_8() {
 function vol4_9() {
   const volume4_9 = document.getElementById('volume4_9');
   const checkVol4_9 = document.getElementById('checkVol4_9');
+
   if (checkVol4_9.checked == true) {
     volume4_9.classList.add('d-block');
     volume4_9.classList.remove('d-none');
+    volume4_9.style.height="50%";
   } else {
     volume4_9.classList.add('d-none');
     volume4_9.classList.remove('d-block');
@@ -110,9 +128,11 @@ function vol4_9() {
 function vol4_10() {
   const volume4_10 = document.getElementById('volume4_10');
   const checkVol4_10 = document.getElementById('checkVol4_10');
+
   if (checkVol4_10.checked == true) {
     volume4_10.classList.add('d-block');
     volume4_10.classList.remove('d-none');
+    volume4_10.style.height="55%";
   } else {
     volume4_10.classList.add('d-none');
     volume4_10.classList.remove('d-block');
@@ -122,9 +142,11 @@ function vol4_10() {
 function vol4_11() {
   const volume4_11 = document.getElementById('volume4_11');
   const checkVol4_11 = document.getElementById('checkVol4_11');
+
   if (checkVol4_11.checked == true) {
     volume4_11.classList.add('d-block');
     volume4_11.classList.remove('d-none');
+    volume4_11.style.height="10%";
   } else {
     volume4_11.classList.add('d-none');
     volume4_11.classList.remove('d-block');
@@ -134,9 +156,11 @@ function vol4_11() {
 function vol4_12() {
   const volume4_12 = document.getElementById('volume4_12');
   const checkVol4_12 = document.getElementById('checkVol4_12');
+
   if (checkVol4_12.checked == true) {
     volume4_12.classList.add('d-block');
     volume4_12.classList.remove('d-none');
+    volume4_12.style.height="65%";
   } else {
     volume4_12.classList.add('d-none');
     volume4_12.classList.remove('d-block');
@@ -146,9 +170,11 @@ function vol4_12() {
 function vol5_1() {
   const volume5_1 = document.getElementById('volume5_1');
   const checkVol5_1 = document.getElementById('checkVol5_1');
+
   if (checkVol5_1.checked == true) {
     volume5_1.classList.add('d-block');
     volume5_1.classList.remove('d-none');
+    volume5_1.style.height="40%";
   } else {
     volume5_1.classList.add('d-none');
     volume5_1.classList.remove('d-block');
@@ -158,9 +184,11 @@ function vol5_1() {
 function vol5_2() {
   const volume5_2 = document.getElementById('volume5_2');
   const checkVol5_2 = document.getElementById('checkVol5_2');
+
   if (checkVol5_2.checked == true) {
     volume5_2.classList.add('d-block');
     volume5_2.classList.remove('d-none');
+    volume5_2.style.height="5%";
   } else {
     volume5_2.classList.add('d-none');
     volume5_2.classList.remove('d-block');
@@ -170,9 +198,11 @@ function vol5_2() {
 function vol5_3() {
   const volume5_3 = document.getElementById('volume5_3');
   const checkVol5_3 = document.getElementById('checkVol5_3');
+
   if (checkVol5_3.checked == true) {
     volume5_3.classList.add('d-block');
     volume5_3.classList.remove('d-none');
+    volume5_3.style.height="35%";
   } else {
     volume5_3.classList.add('d-none');
     volume5_3.classList.remove('d-block');
@@ -182,9 +212,11 @@ function vol5_3() {
 function vol5_4() {
   const volume5_4 = document.getElementById('volume5_4');
   const checkVol5_4 = document.getElementById('checkVol5_4');
+
   if (checkVol5_4.checked == true) {
     volume5_4.classList.add('d-block');
     volume5_4.classList.remove('d-none');
+    volume5_4.style.height="55%";
   } else {
     volume5_4.classList.add('d-none');
     volume5_4.classList.remove('d-block');
@@ -194,9 +226,11 @@ function vol5_4() {
 function vol5_5() {
   const volume5_5 = document.getElementById('volume5_5');
   const checkVol5_5 = document.getElementById('checkVol5_5');
+
   if (checkVol5_5.checked == true) {
     volume5_5.classList.add('d-block');
     volume5_5.classList.remove('d-none');
+    volume5_5.style.height="30%";
   } else {
     volume5_5.classList.add('d-none');
     volume5_5.classList.remove('d-block');
@@ -206,30 +240,14 @@ function vol5_5() {
 function vol5_6() {
   const volume5_6 = document.getElementById('volume5_6');
   const checkVol5_6 = document.getElementById('checkVol5_6');
+
   if (checkVol5_6.checked == true) {
     volume5_6.classList.add('d-block');
     volume5_6.classList.remove('d-none');
+    volume5_6.style.height="85%";
   } else {
     volume5_6.classList.add('d-none');
     volume5_6.classList.remove('d-block');
-  }
-}
-
-function zoom() {
-  const svgZoom4 = document.getElementById('svg_4_zoom');
-  const groupZoom4 = document.getElementById('group_4_zoom');
-
-  if (svgZoom4.checked == true) {
-    groupZoom4.classList.add('d-block');
-  }
-
-  function zoom() {
-    const svgZoom5 = document.getElementById('svg_5_zoom');
-    const groupZoom5 = document.getElementById('group_5_zoom');
-
-    if (svgZoom5.checked == true) {
-      groupZoom5.classList.add('d-block');
-    }
   }
 }
 
@@ -237,7 +255,6 @@ document.addEventListener('DOMContentLoaded', function () {
   const svgZoom4 = document.getElementById('svg_4_zoom');
   const groupZoom4 = document.getElementById('group_4_zoom');
   const groupHidden4 = document.getElementById('group_4_hidden');
-  const groupLabel4 = document.getElementById('group_4_label');
 
   const svgZoom5 = document.getElementById('svg_5_zoom');
   const groupZoom5 = document.getElementById('group_5_zoom');
